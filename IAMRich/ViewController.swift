1e23825faee9c67cc69450345dc3e1c7d478de92
NateRobinson <840501291@qq.com>
2019-03-28 16:54:48 +0800

    Initial Commit

---
+++ IAMRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAMRich
+//
+//  Created by  NateRobinson on 2019/3/28.
+//  Copyright © 2019  NateRobinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
